c2263fedc420d13937cd58db846050eccc12958b
mielvitug <atvitug1@gmail.com>
2026-04-15 20:17:16 +0800

    Updated and added json file

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # Prototype_1-Expense-Tracker-CLI
+
+# Load at start
+import json
+
+try:
+    with open("expenses_data.json", "r") as file: # read file
+        expenses = json.load(file)
+except FileNotFoundError:
+    expenses = []
+
 # Add Expenses
 expenses = [] 
 while True:
@@ -12,10 +22,16 @@ while True:
     except:
         print("--------------------\nWrong input. Try again.")
         continue
+
     if choice == 1:
         name = str(input("Name: "))
         amount = float(input("Amount: "))
         expenses.append([name, amount]) # 0 and 1 value in position of the parameter
+
+        with open("expenses_data.json", "w") as file: # overwrites the file
+            json.dump(expenses, file)
+        print("Expenses added.")
+
 # View all expenses
     elif choice == 2:
         if not expenses:
@@ -39,6 +55,10 @@ while True:
 
             remove_num = int(input("Enter the number you want to remove: "))
             removed_num = expenses.pop(remove_num - 1)
+
+            with open("expenses_data.json", "w") as file: # overwrites the expenses_data file
+                json.dump(expenses, file)
+
             print(f"Removed expense: {removed_num}")
 # Exit 
     elif choice == 5:
