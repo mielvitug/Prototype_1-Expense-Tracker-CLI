483ccfaef72a84c2e2f74d3063a9906caa6b3cae
mielvitug <atvitug1@gmail.com>
2026-04-15 19:05:50 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# Add Expenses
+expenses = [] 
+while True:
+    print("====================\n1. Add")
+    print("2. View")
+    print("3. Total")
+    print("4. Remove")
+    print("5. Exit")
+    try: 
+        choice = int(input("====================\nChoose: "))
+    except:
+        print("--------------------\nWrong input. Try again.")
+        continue
+    if choice == 1:
+        name = str(input("Name: "))
+        amount = float(input("Amount: "))
+        expenses.append([name, amount]) # 0 and 1 value in position of the parameter
+# View all expenses
+    elif choice == 2:
+        if not expenses:
+            print("\n--------------------\nNo expenses yet. Try again.")
+        else:
+            for expense in expenses:
+                print(f"\n====================\nHere are your expenses: {expense}")
+# See total spending 
+    elif choice == 3:
+        total = 0
+        for expense in expenses:
+            total += expense[1]
+        print(f"\n====================\n{total} is your total.")
+# Remove expense
+    elif choice == 4:
+        if not expenses:
+            print("\n--------------------\nNo expenses yet. Try again.")
+        else:
+            for i in range(len(expenses)):
+                print(i + 1, expenses[i])
+
+            remove_num = int(input("Enter the number you want to remove: "))
+            removed_num = expenses.pop(remove_num - 1)
+            print(f"Removed expense: {removed_num}")
+# Exit 
+    elif choice == 5:
+        print("Thank you for using the Expense Tracker CLI!")
+        break
